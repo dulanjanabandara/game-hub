62b856ba0f47476cbced474d3f93981fbd07eacd
Dulanjana Bandara <dulanjana.b@fuelback.io>
2024-05-19 13:33:12 +0530

    Displayed genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,27 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
 import useGenres, { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   // const { data: genres, error, isLoading } = useData<Genre>("/genres");
   const { data: genres, error, isLoading } = useGenres();
 
   return (
-    <ul>
+    <List>
       {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id}>
+          <HStack paddingY="5px">
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
