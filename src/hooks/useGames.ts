e504b7b73f2ae8748838a81a10e33ed36100b916
Dulanjana Bandara <dulanjana.b@fuelback.io>
2024-05-20 01:12:59 +0530

    Sorted the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -64,6 +64,7 @@ const useGames = (
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
       },
     },
     [gameQuery]
